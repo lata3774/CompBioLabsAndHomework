61445b9c3baea7e1f63928c493c638fc878b259f
lata3774 <lata3774@colorado.edu>
2020-02-05 14:06:09 -0700

    added comments

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,6 @@
+#pare csv file down to just time, prey, and predator columns and create a temporary file
 cut -f 2-4 -d , PredPreyData.csv > Lab01finalProblemTEMP.csv
+#combine head (number of rows=1) and tail (last 10 lines) of temp file into final file
 cat Lab01finalProblemTEMP.csv | (head -n 1; tail) > Lab01finalProblem.csv
+#delete temporary file
 rm Lab01finalProblemTEMP.csv
